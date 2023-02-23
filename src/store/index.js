0d21e8f64d54d63a6305a3d7d1c70d7e14f7c363
Kirill <forworkskirill@gmail.com>
2023-02-23 19:52:40 +0500

    add persistor

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,25 @@
-import { createStore , combineReducers} from "redux";
+import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { positionReducer } from "./reducers/reducers-positions";
-import {filterReducer} from '../store/reducers/reducers-filter'
-export const store = createStore(combineReducers({
+import { filterReducer } from "../store/reducers/reducers-filter";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, combineReducers({
     positions: positionReducer,
-    filter: filterReducer
-}))
+    filter: filterReducer,
+  }))
+
+export const store = createStore(
+    persistedReducer
+//   combineReducers({
+//     positions: positionReducer,
+//     filter: filterReducer,
+//   })
+);
+
+export const persistor = persistStore(store)
